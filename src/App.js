4a3c8acb76884bc9a736fcbb5d15cecb1fcd3b31
bryantzhengg <bryant.zhg@gmail.com>
2023-01-07 13:07:53 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import Home from "./routes/Home";
+import Myportfolio from "./routes/Myportfolio";
 
 import {Route, Routes} from "react-router-dom";
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/"  element={<Home/>}  />
+      <Route path="/home" element={<Home/>}  />
+      <Route path="/myportfolio" element={<Myportfolio/>}  />
     </Routes>
     </>
   );
---
+++ src/routes/Myportfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import HeroImage from '../components/HeroImage';
+import Card1 from '../components/Card1';
+
+export const Myportfolio = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HeroImage/>
+      <Card1/>
+    </div>
+  )
+};
+
+export default Myportfolio;
